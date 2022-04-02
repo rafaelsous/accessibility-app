34f723215522f67be68c766b67db40363c7ecadb
Rafael Sousa <rafaelsousa.souza@gmail.com>
2022-04-02 19:56:40 -0300

    Aplicando acessibilidade com os atributos 'accessibilityHInt', 'accessibilityState' e 'accessibilityValue'.

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,8 +1,7 @@
+import { Feather } from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-
-import { Container, Input, Button } from './styles';
+import { Button, Container, Input } from './styles';
 
 export function Search() {
   const [loading, setLoading] = useState(false);
@@ -14,21 +13,25 @@ export function Search() {
   }, [loading]);
 
   return (
-
     <Container>
-      <Input
-        placeholder="Encontrar partida..."
-      />
+      <Input placeholder="Encontrar partida..." />
 
       <Button
         onPress={() => setLoading(!loading)}
         disabled={loading}
+        accessibilityHint="Buscar partida pelo nome do time"
+        accessibilityState={{ disabled: loading }}
+        accessibilityValue={{
+          text: loading
+            ? 'Carregando partidas, aguarde.'
+            : 'Botão de pesquisa disponível novamente.',
+        }}
       >
-        {
-          loading
-            ? <ActivityIndicator color="#FFF" />
-            : <Feather name="search" size={18} color="#FFF" />
-        }
+        {loading ? (
+          <ActivityIndicator color="#FFF" />
+        ) : (
+          <Feather name="search" size={18} color="#FFF" />
+        )}
       </Button>
     </Container>
   );
